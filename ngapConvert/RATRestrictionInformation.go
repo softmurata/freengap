49416f24bd5c33b10f341a3683d525eb8a2abf81
softmurata <tatsuromurata317@gmail.com>
2022-05-27 06:48:15 +0000

    change models to softmurata

---
+++ ngapConvert/RATRestrictionInformation.go
@@ -0,0 +1,25 @@
+package ngapConvert
+
+import (
+	"github.com/free5gc/aper"
+	"github.com/softmurata/freengap/ngapType"
+	"github.com/softmurata/freeopenapi/models"
+)
+
+// TS 38.413 9.3.1.85
+func RATRestrictionInformationToNgap(ratType models.RatType) (ratResInfo ngapType.RATRestrictionInformation) {
+	// Only support EUTRA & NR in version15.2.0
+	switch ratType {
+	case models.RatType_EUTRA:
+		ratResInfo.Value = aper.BitString{
+			Bytes:     []byte{0x80},
+			BitLength: 8,
+		}
+	case models.RatType_NR:
+		ratResInfo.Value = aper.BitString{
+			Bytes:     []byte{0x40},
+			BitLength: 8,
+		}
+	}
+	return
+}
